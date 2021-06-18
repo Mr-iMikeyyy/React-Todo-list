026a39c7c89c82c6c68ac96c580cf6f3aee410fb
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-06-18 18:37:50 -0400

    most of the hard work done, gotta focus on getting the list to populate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [newListItem, setNewListItem] = useState("");
+  const [listArray, setListArray] = useState([]);
+  const [listHtml, setListHtml] = useState("");
+
+  function populateList() {
+    for (var i = 0; i <= listArray.length; i++) {
+      if (listArray[i] !== undefined && listArray[i] !== null) {
+        const newHtml = "<li>" + listArray[i] + "</li>";
+
+        console.log("BEFORE populate::::::::::::::::::::", newHtml);
+        setListHtml((prevValue) => {
+          return prevValue + newHtml;
+        });
+        console.log("AFTER populate::::::::::::::::::::", listHtml);
+      }
+    }
+  }
+
+  function handleClick() {
+    const newValue = newListItem;
+    setListArray((prevValue) => {
+      return [...prevValue, newValue];
+    });
+    setNewListItem("");
+    populateList();
+  }
+
+  function handleChange(event) {
+    const inputValue = event.target.value;
+    setNewListItem(inputValue);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} value={newListItem} type="text" />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
-        <ul>
-          <li>A Item </li>
-        </ul>
+        <ul>{listHtml}</ul>
       </div>
     </div>
   );
